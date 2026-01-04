ef41f0bea32a2b9374c4c63d404e91c903829277
postiglione127-star <postiglione127@gmail.com>
2026-01-04 22:16:44 +0100

    Completed home page and project tasks

---
+++ home.py
@@ -0,0 +1,74 @@
+import streamlit as st
+import base64
+
+    
+# ----- Page configs (tab title, favicon) -----
+st.set_page_config(
+    page_title="Alessandro Postiglione Portfolio",
+    page_icon="📊",
+)
+
+
+def home_page():
+    # ----- Left menu -----
+    with st.sidebar:
+        st.image("eae_img.png", width=200)
+        st.header("Introduction to Programming Languages for Data")
+        st.write("###")
+        st.write("***Final Project - Dec 2025***")
+        st.write("**Author:** Alessandro Postiglione ")
+        st.write("**Instructor:** [Enric Domingo](https://github.com/enricd)")
+
+
+    # ----- Top title -----
+    st.html("""<div style="text-align: center;"><h1 style="text-align: center;">👋 Hi! My name is Alessandro Postiglione</h1></div>""")  # TODO: Add your name
+
+
+    # ----- Profile image file -----
+    profile_image_file_path = "profile.png"       # TODO: Upload your profile image to the same folder as this script and update this if it has a different name
+
+    with open(profile_image_file_path, "rb") as img_file:
+        img = "data:image/png;base64," + base64.b64encode(img_file.read()).decode()
+
+
+    # ----- Your Profile Image -----
+    st.html(f"""
+    <div style="display: flex; justify-content: center;">
+        <img src="{img}" alt="Your Name" width="300" height="300" style="border-radius: 50%; object-fit: cover; margin-top: 40px; margin-bottom: 40px;">
+    </div>
+    """)
+
+
+    # ----- Personal title or short description -----
+    current_role = "Master in Big Data & Analytics"   
+
+    st.html(f"""<div style="text-align: center;"><h4><i>{current_role}</i></h4></div>""")
+
+    st.write("#  #")    # Adding some space
+
+
+    # ----- About me section -----
+    st.subheader("About Me")
+
+    # TODO: Modify and adapt the following lines to your info, you can add or remove some details if you want
+    st.write("""
+    - 🧑‍💻 I'm attending a Master in Big Data & Analytics at EAE Business School
+
+    - 🛩️ prev: <Master's Degree in Business Administration>
+
+    - 📫 How to reach me: postiglione127@gmail.com
+
+    - 🏠 Barcelona
+    """)
+
+
+
+
+# This is ensambling the entire app with the different pages and the navigation menu
+pg = st.navigation([
+    st.Page(home_page, title="Home", icon="👋"),
+    st.Page("pages/01_image_cropper.py", title="Image Cropper", icon="🖼️"),
+    st.Page("pages/02_netflix_data_analysis.py", title="Netflix Data Analysis", icon="🎬"),
+    st.Page("pages/03_temperatures_dashboard.py", title="Temperatures Dashboard", icon="🌦️"),
+])
+pg.run()
